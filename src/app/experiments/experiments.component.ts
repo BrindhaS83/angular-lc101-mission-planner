551d9b436b87bfa4c03af472e8f84ddc923f414b
Vetri Kaliyamurthy <VKaliyamurthy@tvsscs.com>
2021-08-10 12:32:44 -0500

    Studio Bonus mission done

--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiments.component.css']
 })
 export class ExperimentsComponent implements OnInit {
-
+  experiment: string[] =["Mars soil sample","Plant growth in habitat","Human bone density"];
+  
+  experimentBeingEdited: string = null;
   constructor() { }
 
   ngOnInit() {
   }
+  add(experimentName: string) {
+            
+    this.experiment.push(experimentName);
+  }
+  remove(experimentName: string) {
 
+    let index = this.experiment.indexOf(experimentName);
+    this.experiment.splice(index, 1);
+  
+  }
+  edit(experi: string) {
+    this.experimentBeingEdited = experi;
+ }
+ save(oldExperimentName: string, newExperimentName: string){
+   let index = this.experiment.indexOf(oldExperimentName)
+   this.experiment[index] = newExperimentName;
+   this.experimentBeingEdited =null;
+ }
 }
